2aa1b0954ec54b290e77f751b510ff40a2883931
e <3055964@qq.com>
2020-06-30 22:32:43 +0800

    log

---
+++ Learun.Framework.Module/Learun.Log/LogLevel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+namespace Learun.Loger
+{
+    /// <summary>
+    
+    /// Copyright (c) 2013-2020 上海力软信息技术有限公司
+    /// 创建人：力软-框架开发组
+    /// 日 期：2017.03.04
+    /// 描 述：日志级别
+    /// </summary>
+    public enum LogLevel
+    {
+        /// <summary>
+        /// 错误
+        /// </summary>
+        [Description("错误")]
+        Error,
+        /// <summary>
+        /// 警告
+        /// </summary>
+        [Description("警告")]
+        Warning,
+        /// <summary>
+        /// 信息
+        /// </summary>
+        [Description("信息")]
+        Info,
+        /// <summary>
+        /// 调试
+        /// </summary>
+        [Description("调试")]
+        Debug
+    }
+}
